2401948a48d0194cfa536e7df165b1b96a781b3e
Faizullah032 <faizullahlaghari032@gmail.com>
2025-01-02 12:44:16 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+
+gsap.from(".nav-item,.navbar-brand,.trial", {
+    y: -80,
+    opacity: 0,
+    duration: 1.2,
+    delay: 0.8,
+    stagger: 0.2
+});
+
+gsap.from(".header h1,.header p", {
+    x: -500,
+    opacity: 0,
+    duration: 1.2,
+    delay: 0.8,
+    stagger: 0.5
+});
+
+gsap.from(".header .image-container", {
+    x: 500,
+    opacity: 0,
+    duration: 1.2,
+    delay: 0.8,
+});
+
+gsap.from(".download", {
+    y: 500,
+    opacity: 0,
+    duration: 1.2,
+    delay: 0.8,
+});
+
+
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".about",
+        scroller: "body", 
+        start: "top 70%",
+        end: "top 10%",
+        scrub: 2,
+    }
+});
+
+tl.from(".about .image-container", {
+    x: 500,
+    y: 200,
+    opacity: 0,
+    duration: 1.5,
+    delay: 1.5,
+});
+
+tl.from(".about h1,.about p", {
+    x: -300,
+    duration: 2.5,
+    delay: 1.5,
+    opacity: 0,
+    stagger: 1.5
+});
+
+
+tl.from(".data-science h1, .data-science p", {
+    y: 500,
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0,
+    stagger: 0.5,
+},"=2");
